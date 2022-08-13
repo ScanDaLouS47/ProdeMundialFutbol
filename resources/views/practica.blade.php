d79b6bafa50581ec2f9dfb0219d74f347f83dd35
Angelovilla22 <angelvillafane75@gmail.com>
2022-08-12 21:30:39 -0300

    probando boostrap

---
+++ resources/views/practica.blade.php
@@ -0,0 +1,25 @@
+@extends('plantilla')
+
+@section('titulo', 'Titulo')
+
+@section('contenido')
+
+<div class="row coloresPrincipales">
+    <div class="col-md-3">
+      <button type="button" class="btn btn-outline-danger">Prode <img class="iconos"src="https://img.icons8.com/ios/50/000000/calendar--v1.png"/></button> 
+      </div>
+      <div class="col-md-3">
+        <button type="button" class="btn btn-outline-danger">Ranking <img class="iconos" src="https://img.icons8.com/external-xnimrodx-lineal-xnimrodx/64/000000/external-ranking-game-development-xnimrodx-lineal-xnimrodx.png"/></button>  
+      </div>
+      <div class="col-md-3">
+        <button type="button" class="btn btn-outline-danger">Fixture <img src="img/calendario.png"/></button> 
+      </div>
+      <div class="col-md-3">
+        <button type="button" class="btn btn-outline-danger">Usuario <img class="iconos" src="https://img.icons8.com/ios-filled/50/000000/username.png"/></i></button>        
+      </div>
+
+  </div>
+  
+
+
+@endsection
