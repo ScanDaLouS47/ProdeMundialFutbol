71affa498605345a6ab34219216ddf8a45c934f3
Angelovilla22 <angelvillafane75@gmail.com>
2022-08-15 21:40:52 -0300

    creandoMenuHome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,32 @@
     <head>
         @yield('imports', View('imports'))
         
-        <title>Laravel</title>        
+        <title>ProdeMundial</title>        
 
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
             }
+
+            a {
+                text-decoration: none;
+                color: #F3F3F5;
+            }
+            
+            a:hover {
+                color: #F3F3F5;
+                text-decoration: underline;
+                font-weight: bold;
+            }
+            .fondoMenu {
+                background-color: #8c183d;
+            }
+        
+            
         </style>
     </head>
     <body class="antialiased">
-        @yield('menu', View('menu'))
+        @yield('menuhome', View('menuhome'))
 
         <p>Contenido</p>
 
---
+++ resources/views/menuhome.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg fondoMenu">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"><img src="img/toggle.png" alt=""></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+     
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item ">
+              <a class="nav-link" href="#"> <img src="img/botonRegistro.png" alt=""> Registrarse</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><img src="img/botonEntrar.png" alt=""> Entrar</a>
+              </li>
+        </ul>
+    </div>
+  </nav>
