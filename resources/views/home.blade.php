f22bb94c5d87b86b0c6d1b0383d1695a257ae94c
Angelovilla22 <angelvillafane75@gmail.com>
2022-08-16 21:28:46 -0300

    creando menu y footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,32 +5,13 @@
         
         <title>ProdeMundial</title>        
 
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-
-            a {
-                text-decoration: none;
-                color: #F3F3F5;
-            }
-            
-            a:hover {
-                color: #F3F3F5;
-                text-decoration: underline;
-                font-weight: bold;
-            }
-            .fondoMenu {
-                background-color: #8c183d;
-            }
         
-            
-        </style>
     </head>
-    <body class="antialiased">
-        @yield('menuhome', View('menuhome'))
-
-        <p>Contenido</p>
+    <body class="antialiased">        
+        @yield('menu', View('menu'))        
+        <div class="row">
+            <p>Contenido</p>
+        </div>
 
         @yield('footer', View('footer'))
     </body>
