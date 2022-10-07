e7c7a543bbb2a715029713976345ea46a161eb74
Maico <mbeas@inxap.com>
2022-10-07 18:26:15 -0300

    agregando view ranking

---
+++ resources/views/ranking.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        @yield('imports', View('imports'))
+        
+        <title>ProdeMundial</title>        
+
+        
+    </head>
+    <body class="antialiased">        
+        @yield('menu', View('menu'))  
+        <br>             
+        
+            
+        @yield('footer', View('footer'))
+    </body>
+</html>
