42b347d9e45a5b23250edbfe62fd846e071d6ed3
Maico <mbeas@inxap.com>
2022-10-12 16:48:19 -0300

    route

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -41,7 +41,7 @@
             </div>
             <div class="col-md"></div>            
         </div>
-        @if(url()->previous()->getName() == 'registro' && session('resp'))                          
+        @if(url()->previous() == '{{ route('registro') }}' && session('resp'))                          
             @if(session('resp') == 'ok')
               <script>
                 
