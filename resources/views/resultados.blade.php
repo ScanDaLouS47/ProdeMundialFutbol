85161f5cfd4ff9995b55d28a09084e18a5efdc44
Angelovilla22 <angelvillafane75@gmail.com>
2022-10-15 15:21:23 -0300

    creando select

--- resources/views/resultados.blade.php
+++ resources/views/resultados.blade.php
@@ -12,7 +12,41 @@
             <div class="col-12 col-md-12 p-0">
                @yield('menuadmin', View('menuadmin'))        
             </div>
-        </div>           
+        </div>  
+        <div class="row">
+            <div class="col-12 col-md"></div>
+            <div class="col-md-4">
+                <p class="text-center textohome">Resultados Partidos</p>
+             </div> 
+            <div class="col-md"></div>    
+           
+        </div>  
+        <div class="row">
+            <div class="col-12 col-md"></div>
+            <div class="col-md-4">
+                <select class="custom-select">
+                    <option selected>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+            </div>
+            <div class="col-md"></div>
+        </div>       
+        <br>
+        <div class="row">
+            <div class="col-12 col-md"></div>
+            <div class="col-md-4">
+                <select class="custom-select">
+                    <option selected>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+            </div>
+            <div class="col-md"></div>
+        </div>    
+        <br>
                 
 
         @yield('footer', View('footer'))
