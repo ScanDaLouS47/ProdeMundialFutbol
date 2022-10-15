d7ea8f049d3576380427c887317309d96ca0bf12
Angelovilla22 <angelvillafane75@gmail.com>
2022-10-15 17:13:12 -0300

    creandousuarios

--- resources/views/resultados.blade.php
+++ resources/views/resultados.blade.php
@@ -22,8 +22,8 @@
            
         </div>  
         <div class="row">
-            <div class="col-12 col-md"></div>
-            <div class="col-md-4">
+            <div class="col-md"></div>
+            <div class="col-12 col-md-4">
                 <select class="custom-select">
                     <option value="0" selected>-Seleccione el partido-</option>                   
                     @foreach ($partidos as $partido)   
@@ -38,8 +38,8 @@
         </div>       
         <br>
         <div class="row">
-            <div class="col-12 col-md"></div>
-            <div class="col-md-4">
+            <div class="col-md"></div>
+            <div class="col-12 col-md-4">
                 <select class="custom-select">
                     <option selected>Open this select menu</option>
                     <option value="1">One</option>
@@ -50,7 +50,65 @@
             <div class="col-md"></div>
         </div>    
         <br>
-                
+        <div class="row">
+            <div class="col-md"></div>
+            <div class="col-12 col-md-4">
+                <button class="btn btn-block text-center textohome btn-primary">Guardar partido</button>
+            </div>
+            <div class="col-md"></div>
+        </div>
+        <br>
+        <hr>    
+        <div class="row">
+            <div class="col-md"></div>
+            <div class="col-12 col-md-4">
+                <p class="text-center textohome">Resultados Cargados</p>
+            </div>
+            <div class="col-md"></div>
+        </div>
+        <div class="row">
+            <div class="col-md"></div>
+            <div class="col-12 col-md-6">
+                <table class="table table-striped table-dark">
+                    <thead>
+                      <tr>
+                        <th scope="col">fecha</th>
+                        <th scope="col">Equipo 1</th>
+                        <th scope="col">Equipo 2</th>
+                        <th scope="col">Resultado</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">1</th>
+                        <td>Argentina</td>
+                        <td>Polonia</td>
+                        <td>Argentina(3-2)</td>
+                        
+                      </tr>
+                      <tr>
+                        <th scope="row">2</th>
+                        <td>Francia</td>
+                        <td>Dinamarca</td>
+                        <td>Francia(3-2)</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">3</th>
+                        <td>Brazil</td>
+                        <td>Suiza</td>
+                        <td>Brazil(2-1)</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">4</th>
+                        <td>Brazil</td>
+                        <td>Camerun</td>
+                        <td>Brazil(3-1)</td>
+                      </tr>
+                    </tbody>
+                  </table>
+            </div>
+            <div class="col-md"></div>
+        </div>
 
         @yield('footer', View('footer'))
     </body>
--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -12,8 +12,9 @@
             <div class="col-12 col-md-12 p-0">
                @yield('menuadmin', View('menuadmin'))        
             </div>
-         </div>           
-        <p>Usuarios registrados</p>     
+         </div>   
+         <br>        
+        <p class="text-center textohome">Usuarios registrados</p>     
         <ul>                
             @foreach ($usuarios as $usuario)
                 <li>{{ $usuario->nombre }}</li>
@@ -22,8 +23,58 @@
         @if(session('resp'))
             <p>El mensaje es : {{ session('message') }}</p>        
         @endif
-                
+        <table class="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Usuario</th>
+                <th scope="col">Nombre y Apellido</th>
+                <th scope="col">Dni</th>
+                <th scope="col">Cantidad de Prodes</th>
+                <th scope="col">Modificar</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Marcos Gil</td>
+                <td>35893816</td>
+                <td>1</td>
+                <td><i class="fa fa-user-edit"></i></td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob Vagio</td>
+                <td>23987357</td>
+                <td>2</td>
+                <td><i class="fa fa-user-edit"></i></td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Vanesa Rosso</td>
+                <td>16893835</td>
+                <td>1</td>
+                <td><i class="fa fa-user-edit"></i></td>
+              </tr>
+              <tr>
+                <th scope="row">4</th>
+                <td>Julio Vignolo</td>
+                <td>35987357</td>
+                <td>3</td>
+                <td><i class="fa fa-user-edit"></i></td>
+              </tr>
+              <tr>
+                <th scope="row">5</th>
+                <td>Carlos Beltran</td>
+                <td>15369896</td>
+                <td>1</td>
+                <td><i class="fa fa-user-edit"></i></td>
+              </tr>
+            </tbody>
+          </table>
+          <hr>
 
         @yield('footer', View('footer'))
+
+        
     </body>
 </html>
