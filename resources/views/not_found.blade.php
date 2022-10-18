815abf5332a2154946e0d6d7364cc0da4faa21bf
Maico <mbeas@inxap.com>
2022-10-18 19:12:09 -0300

    agregando ruta not_found

---
+++ resources/views/not_found.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>        
+        
+        <title>ProdeMundial</title>        
+
+        
+    </head>
+    <body class="antialiased">        
+
+    </body>
+</html>
