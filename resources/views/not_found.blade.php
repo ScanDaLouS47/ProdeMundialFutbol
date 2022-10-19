526d3b5bbbdc7a0b90b3f67e51b766123906b26d
Angelovilla22 <angelvillafane75@gmail.com>
2022-10-19 19:46:51 -0300

    creando vista error 404

--- resources/views/not_found.blade.php
+++ resources/views/not_found.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>        
-        
+        @yield('imports', View('imports'))
         <title>ProdeMundial</title>        
 
         
     </head>
     <body class="antialiased">        
-
+        <img src="./img/error.gif" class="imgHome">
     </body>
 </html>
