e34abfc7dee584854c8808978bb5339600f43cb8
Angelovilla22 <angelvillafane75@gmail.com>
2022-10-20 06:46:02 -0300

    cambiando vista resultados

--- resources/views/resultados.blade.php
+++ resources/views/resultados.blade.php
@@ -40,12 +40,14 @@
         <div class="row">
             <div class="col-md"></div>
             <div class="col-12 col-md-4">
-                <select class="custom-select">
-                    <option selected>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                  </select>
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <input placeholder="Equipo 1" type="number" style="width: 100%" name="Equipo1" value="{{ old('Equipo1') }}">
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <input placeholder="Equipo 2" type="number" style="width: 100%" name="Equipo1" value="{{ old('Equipo1') }}">
+                    </div>
+                </div>
             </div>
             <div class="col-md"></div>
         </div>    
